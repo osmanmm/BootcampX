fc06e09c2494b2e26cceff74892153323ead5607
osmanmm <mohamed.osman@outlook.com>
2021-10-02 16:33:01 +0000

    total students in cohort

--- 3_queries_group-by/3_total_students_in_cohort.sql
+++ 3_queries_group-by/3_total_students_in_cohort.sql
@@ -1,6 +1,6 @@
 SELECT cohorts.name, count(*) AS total_students
 FROM cohorts
-LEFT OUTER JOIN students ON cohort_id = cohorts.id
+LEFT OUTER JOIN  students ON cohort_id = cohorts.id
 GROUP BY cohorts.name
 HAVING count(*) >= 18
 ORDER BY total_students;
